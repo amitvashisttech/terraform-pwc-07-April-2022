0974bc9b7227d2450d1efbd7d9da3643a3f12dc0
Amit Vashist <amitvashist7@outlook.com>
2022-04-07 10:10:53 +0000

    03-Resources

---
+++ 03-Resources/exp5-prevent-destroy/main.tf
@@ -0,0 +1,41 @@
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "frontend" {
+  depends_on = [ azurerm_resource_group.backend ]
+  count    = 1
+  name     = "frontend-RG-${count.index + 1}"
+  location = "eastus"
+
+  tags = {
+    Owner   = "Amit Vashist"
+    Team    = "DevOps Team"
+    Project = "Terraform Automation"
+  }
+
+}
+
+
+resource "azurerm_resource_group" "backend" {
+  count    = 1
+  name     = "backend-RG-${count.index + 1}"
+  location = "eastus"
+
+  tags = {
+    Owner   = "Amit Vashist"
+    Team    = "DevOps Team"
+    Project = "Terraform Automation"
+  }
+
+ lifecycle { 
+  prevent_destroy = false 
+ }
+
+}
+
+
+
+
+
