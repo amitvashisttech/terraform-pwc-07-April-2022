fc8c5d3f764b766a67ca466f29d6b61e96cfbed2
amitvashisttech <57354049+amitvashisttech@users.noreply.github.com>
2022-04-12 12:58:45 +0530

    Update network.tf
    RG Definition has been corrected

--- 17-Modules/exp1/modules/network/network.tf
+++ 17-Modules/exp1/modules/network/network.tf
@@ -1,6 +1,6 @@
 # Create a resource group
 resource "azurerm_resource_group" "example_rg" {
-  name     = "${var.my_rg_name}-resources-RG"
+  name     = "${var.my_rg_name}"
   location = var.my_loc
 
   tags = {
