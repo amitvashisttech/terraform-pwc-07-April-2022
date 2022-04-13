55c551a86fe67deb6c2edf51d96a2543076691a8
Amit Vashist <amitvashist7@outlook.com>
2022-04-13 05:28:38 +0000

    19-Backend-and-Remote-State

---
+++ 19-Backend-and-Remote-State/Local/problem/vashist/main.tf
@@ -0,0 +1,12 @@
+# Create a Resource Group
+resource "azurerm_resource_group" "blue" {
+  count    = 1 
+  name     = "red-RG-${count.index +1}"
+  location = "eastus"
+  tags = {
+    Owner = "Amit Vashist" 
+    Team  = "DevOps Team"
+    Project = "Terraform Automation"
+  }
+}
+
