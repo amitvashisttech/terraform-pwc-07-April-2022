5b1099113bd01327727395c9ece91c0e99b61d6b
Amit Vashist <amitvashist7@outlook.com>
2022-04-13 09:47:07 +0000

    20-WorkSpace

---
+++ 20-WorkSpace/Remote/main.tf
@@ -0,0 +1,12 @@
+# Create a Resource Group
+resource "azurerm_resource_group" "blue" {
+  count    = lookup(var.rg_count, terraform.workspace)  
+  name     = "${var.my_rg_name}-tf-${terraform.workspace}-${count.index +1}"
+  location = var.my_loc
+  tags = {
+    Owner = "Amit Vashist" 
+    Team  = "DevOps Team"
+    Project = "Terraform Automation - ${terraform.workspace}"
+  }
+}
+
