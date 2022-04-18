680a4baefb0dc21a4f7013ca391d991c93160e98
Amit Vashist <amitvashist7@outlook.com>
2022-04-18 05:09:08 +0000

    24-Ansible-Azure-Machine-SSH

---
+++ Terraform/23-Provisioner-File-Exec/terraform.tf
@@ -0,0 +1,10 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+
+}
